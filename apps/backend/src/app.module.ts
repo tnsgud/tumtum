c2f86c77194649ac1cf5f3ab50d8776bcb4e9fe5
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-04-20 13:28:25 +0000

    ✨ Connect prisma module and mission module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common'
 import { MissionModule } from './mission/mission.module'
+import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
-  imports: [MissionModule],
+  imports: [PrismaModule, MissionModule],
 })
 export class AppModule {}
