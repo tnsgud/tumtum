bdaa64525c25c02deb3ae6682533aaa7ef0da2a8
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-04-20 13:27:19 +0000

    🎨 Format code

--- apps/backend/src/prisma/prisma.module.ts
+++ apps/backend/src/prisma/prisma.module.ts
@@ -1,6 +1,6 @@
-import { Global, Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
-import { MissionModule } from 'src/mission/mission.module';
+import { Global, Module } from '@nestjs/common'
+import { PrismaService } from './prisma.service'
+import { MissionModule } from 'src/mission/mission.module'
 
 @Global()
 @Module({
