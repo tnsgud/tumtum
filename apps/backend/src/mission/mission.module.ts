d5a617ad009da0dff21326b34b0dffaf68adb158
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-04-21 08:07:27 +0000

    🤡 test

--- apps/backend/src/mission/mission.module.ts
+++ apps/backend/src/mission/mission.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common'
 import { MissionService } from './mission.service'
 import { MissionController } from './mission.controller'
 
-const helllo = 'this is test'
-
 @Module({
   controllers: [MissionController],
   providers: [MissionService],
