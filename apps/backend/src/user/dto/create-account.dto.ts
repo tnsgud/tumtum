0e29372f72ff8ddb72f7dfeb49793b9fd77a60c0
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-05-13 14:38:35 +0000

    🚚 Rename to create-account

--- apps/backend/src/user/dto/create-account.dto.ts
+++ apps/backend/src/user/dto/create-account.dto.ts
@@ -3,22 +3,31 @@ import {
   PASSWORD_LENGTH_ERROR_MESSAGE,
   PASSWORD_REGEX,
   PASSWORD_REGEX_ERROR_MESSAGE,
+  ICreateAccountOutput,
+  ICreateAccountInput,
+  UserErrorCode,
 } from '@tumtum/shared'
 
 import { IsEmail, IsString, Matches, MinLength } from 'class-validator'
-import { CoreOutput } from 'src/common/dto/output.dto'
 
-export class SignupInputDto {
+export class CreateAccountDto implements ICreateAccountInput {
   @IsString()
   username: string
+
+  @IsString()
+  nickname: string
 
   @IsEmail()
   email: string
 
+  // TODO: 현재 response가 output 형식에 맞지 않기에 수정해야함 ex) filter를 써야한다고 함
   @IsString()
   @MinLength(PASSWORD_MIN_LENGTH, { message: PASSWORD_LENGTH_ERROR_MESSAGE })
   @Matches(PASSWORD_REGEX, { message: PASSWORD_REGEX_ERROR_MESSAGE })
   password: string
 }
 
-export class SignupOutput extends CoreOutput {}
+export class CreateAccountOutput implements ICreateAccountOutput {
+  ok: boolean
+  error?: UserErrorCode | undefined
+}
