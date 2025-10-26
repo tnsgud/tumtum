6a2fdf9148a3e22a177d4be867898b3edfd5c993
박순형 <60930113+tnsgud@users.noreply.github.com>
2025-10-26 13:32:51 +0000

    ♻️ store type 분리

--- stores/mission-store.ts
+++ stores/mission-store.ts
@@ -1,34 +1,35 @@
-import { Mission } from '@/components/mission/types';
-import { browserClient } from '@/lib/supabase.browser';
-import { create } from 'zustand';
+import { Mission } from "@/components/mission/types";
+import { browserClient } from "@/lib/supabase.browser";
+import { create } from "zustand";
 
-interface MissionStore {
-  missions: Mission[]
-  setMissions: (v: Mission[]) => void
-  init: boolean
-  refresh: (v?: boolean) => Promise<void>
-}
+type State = {
+	missions: Mission[];
+};
 
-const useMissionStore = create<MissionStore>((set, get) => ({
-  missions: [],
-  setMissions: (v) => set({ missions: v }),
-  init: false,
-  refresh: async (value?: boolean) => {
-    const supabase = browserClient();
-    const {data, error} = await supabase
-      .from('mission')
-      .select(
-        'id, title, deadline_at, is_completed, priority, category(color, name)'
-      )
-      .is('deleted_at', null).order('created_at', {ascending: false});
-    
-    if (data) {
-      set(state => ({
-        missions: data,
-        init: value ?? state.init
-      }))
-    }
-   },
+type Action = {
+	setMissions: (v: Mission[]) => void;
+	refresh: () => Promise<void>;
+};
+
+const useMissionStore = create<State & Action>((set, get) => ({
+	missions: [],
+	setMissions: (v) => set({ missions: v }),
+	refresh: async () => {
+		const supabase = browserClient();
+		const { data, error } = await supabase
+			.from("mission")
+			.select(
+				"id, title, deadline_at, is_completed, priority, category(color, name)",
+			)
+			.is("deleted_at", null)
+			.order("created_at", { ascending: false });
+
+		if (data) {
+			set({
+				missions: data,
+			});
+		}
+	},
 }));
 
-export {useMissionStore}
+export { useMissionStore };
